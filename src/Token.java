b57142d1de11536fc6a6a58fd3b45d36668c5e96
LloydGhettoWolf <lloyd.crawley@gmail.com>
2020-05-01 13:22:16 +0200

    First initial commit of nearly working scanner

---
+++ src/Token.java
@@ -0,0 +1,37 @@
+enum TokenType{
+    // single characters
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // one or two characters
+    BANG, BANG_EQUAL, ASSIGN, EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
+
+public class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString(){
+        return type + " " + lexeme + " " + literal;
+    }
+}
